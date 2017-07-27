7e51aa96653542fdb17ac085648c513a5e08b0d0
Jakie Rice <jakie.rice@gmail.com>
2017-07-26 20:20:35 -0500

    working table values, need to fix row append

--- assets/javascript/employee.js
+++ assets/javascript/employee.js
@@ -26,19 +26,51 @@ $('#addEmployeeButton').click(function (event) {
 
 // var currentMonth = new Date(year, month);
 
-// db.ref('/employees').on('child_added', function (employee) {
-//     var key = employee.key;
 
-//     var
+function toDate(dateStr) {
+    var parts = dateStr.split("/");
+    return new Date(parts[2], parts[0] - 1, parts[1]);
+}
 
-//     var name = employeeTable.cell.html(employee.name);
-//     var role = employeeTable.cell.html(employee.role);
-//     var start = employeeTable.cell.html(employee.start);
-//     var worked = employeeTable.cell.html();
-//     var rate = employeeTable.cell.html(employee.rate);
-//     var billed = employeeTable.cell.html();
+function monthDiff(d1, d2) {
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth();
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+}
 
-//     var employeeRow = employeeTable.row.attr('id', key + 'EmployeeRow');
+db.ref('/employees').on('child_added', function (employee) {
 
 
-// })
+    var currentDate = new Date();
+
+    var employee = employee.toJSON();
+    var key = employee.key;
+
+    var startDate = toDate(employee.start);
+
+    var monthsWorked = monthDiff(startDate, currentDate);
+
+    var totalBilled = (monthsWorked * employee.rate);
+
+    console.log(totalBilled);
+
+    var name = $('<td></td>').append(employee.name);
+    var role = $('<td></td>').append(employee.role);
+    var start = $('<td></td>').append(employee.start);
+    var worked = $('<td></td>').append(monthsWorked);
+    var rate = $('<td></td>').append(employee.rate);
+    var billed = $('<td></td>').append('$' + parseInt(totalBilled).toLocaleString({ style: 'currency', currency: 'USD' }));
+
+    var employeeRow = employeeTable.row.attr('id', key + 'EmployeeRow');
+
+    employeeRow.append(name)
+        .append(role)
+        .append(start)
+        .append(worked)
+        .append(rate)
+        .append(billed);
+
+    employeeTable.table.append(employeeRow);
+})
